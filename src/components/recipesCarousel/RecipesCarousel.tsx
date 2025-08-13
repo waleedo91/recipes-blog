39aa1094ebe8bf46424549e784c0ea200d7e86ac
Waleed <wsaleh1991@gmail.com>
2025-08-13 07:31:21 -0500

    updated mobile design and desktop design for slide descriptions.

--- src/components/recipesCarousel/RecipesCarousel.tsx
+++ src/components/recipesCarousel/RecipesCarousel.tsx
@@ -8,24 +8,24 @@ import "./RecipesCarousel.css";
 const RecipesCarousel = () => {
   return (
     <div>
-      <Carousel className="carousel-container" fade>
+      <Carousel className="carousel-container" interval={null}>
         <Carousel.Item>
           <img className="carousel-image" src={food1} alt="First slide" />
-          <Carousel.Caption>
+          <Carousel.Caption className="caption1 carousel-caption">
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="carousel-image" src={food2} alt="Second Slide" />
-          <Carousel.Caption className="carousel-caption">
+          <Carousel.Caption className="caption2 carousel-caption">
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="carousel-image" src={food3} alt="Third slide" />
-          <Carousel.Caption>
+          <Carousel.Caption className="caption3 carousel-caption">
             <h3>Third slide label</h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
